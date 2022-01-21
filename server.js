8aeb2e7fa0d04584f5e6b90dc3e3f734dbd60b01
Colin Jaffe <balloonasaurus@gmail.com>
2022-01-21 12:31:41 -0500

    Add delete one dino.

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ app.get("/dinos/:id", (request, response) => {
   response.json(dino[id]);
 });
 
+app.delete("/dinos/:id", (request, response) => {
+  const { id } = request.params;
+  if (!dino[id]) {
+    response.redirect("/404");
+
+    return;
+  }
+
+  const removed = dino.splice(id, 1);
+  response.json(removed);
+});
+
 app.get("/404", (_, response) => {
   response.status(404).json({ error: "Resource not found." });
 });
